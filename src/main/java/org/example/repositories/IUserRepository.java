c68f7b3509c4fae730c3d4e59ff125a3380ef0a3
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-11-01 20:54:35 +0200

    Start springBoot

---
+++ src/main/java/org/example/repositories/IUserRepository.java
@@ -0,0 +1,8 @@
+package org.example.repositories;
+
+import org.example.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserEntity, Long> {
+
+}
